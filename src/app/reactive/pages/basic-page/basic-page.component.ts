52341ed2988a4d15ee4c4e7136851c2266b37d56
Alberto <amasgomis@gmail.com>
2024-08-14 08:03:28 +0200

    Mostrar errores de validación en pantalla

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -25,7 +25,10 @@ export class BasicPageComponent {
   });
 
   onSave(): void {
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
     console.log(this.myForm.value);
   }
 }
