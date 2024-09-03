543a112923464ae508c75a9ad8d7235b6acfc62e
Alberto <amasgomis@gmail.com>
2024-09-03 21:59:11 +0200

    métodos de ayuda para los errores

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -10,7 +10,7 @@ import {
   templateUrl: './basic-page.component.html',
   styles: [],
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
   /* public myForm: FormGroup = new FormGroup({
     name: new FormControl(''),
     price: new FormControl(0),
@@ -18,6 +18,34 @@ export class BasicPageComponent {
   });*/
 
   constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm.reset();
+  }
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres`;
+        case 'required':
+          return 'Este campo es requerido';
+      }
+    }
+    return 'Hola mundo';
+  }
+
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(0)]],
